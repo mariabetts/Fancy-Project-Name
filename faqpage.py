a3029237e0ef8171b841332a2d0ae83c5fe725b0
mariabetts <91084214+mariabetts@users.noreply.github.com>
2023-12-10 22:40:19 +0000

    .

--- faqpage.py
+++ faqpage.py
@@ -4,7 +4,7 @@ def app():
 
     st.title("Welcome to the FAQ page")
 
-    st.write("Questions or Comments? Email the creator at 'i gotta create a fake email for this because my university one might be shutdown after upload'")
+    st.write("Questions or Comments? Email the creator at 
+    'i gotta create a fake email for this because my university one might be shutdown after upload'")
 
-    st.ballons()
-    
+  
