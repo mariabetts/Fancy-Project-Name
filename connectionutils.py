893b650bd5433922a4825f5064ab00d2e5b1e2e8
mariabetts <91084214+mariabetts@users.noreply.github.com>
2023-12-10 14:09:24 +0000

    .

--- connectionutils.py
+++ connectionutils.py
@@ -4,6 +4,40 @@ import pandas as pd
 from datetime import datetime
 import requests
 
+#connect to etherscan
+def get_account_balance(api_key, address):
+    base_url = "https://api.etherscan.io/api"
+    params = {
+        "module": "account",
+        "action": "balance",
+        "address": address,
+        "tag": "latest",
+        "apikey": api_key,
+    }
+
+    response = requests.get(base_url, params=params)
+    data = response.json()
+
+    return data["result"]
+
+## get transactions and connect to api w/ key
+def get_transaction_list(api_key, address):
+    base_url = "https://api.etherscan.io/api"
+    api_key = "ZACGG1654HI5ANR3P2XIX558YRQGCR4UI5"
+    params = {
+        "module": "account",
+        "action": "txlist",
+        "address": address,
+        "startblock": 0,
+        "endblock": 99999999,
+        "sort": "asc",
+        "apikey": api_key,
+    }
+
+    response = requests.get(base_url, params=params)
+    data = response.json()
+
+    return data["result"]
 def get_address_info(address):
     base_url = 'https://api.etherscan.io/api'
     api_key = "ZACGG1654HI5ANR3P2XIX558YRQGCR4UI5"
